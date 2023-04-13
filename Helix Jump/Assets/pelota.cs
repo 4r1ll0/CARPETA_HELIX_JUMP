5fcf8e2fc41d14d06ee66ff7a0cbe014ce8a1f3b
Slazanor <73226150+Slazanor@users.noreply.github.com>
2023-04-13 09:28:52 +0100

    13/04/2023

--- Helix Jump/Assets/pelota.cs
+++ Helix Jump/Assets/pelota.cs
@@ -7,17 +7,33 @@ public class pelota : MonoBehaviour
     Rigidbody ballRG;
     [SerializeField] float forceJump = 5f;
     [SerializeField] GameObject trail;
+    //Variable para almacenar altura inicial
+
+
+    //Variable para almacenar altura final
+
+
     void Start()
     {
         //ver videos en youtube de como hacer esto
         ballRG = GetComponent <Rigidbody>();
         trail.SetActive(false);
+
+        //Guardar la altura inicial
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         ballRG.velocity = Vector3.zero;
         ballRG.AddForce(Vector3.up * forceJump, ForceMode.Impulse);
+
+        //Guardar altura final
+
+        //Distancia recorrida = Restar incial - final
+
+        // Mostrar en un log la distancia recorrida
+        
+        
     }
 
 }
